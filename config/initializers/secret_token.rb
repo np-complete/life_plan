cd2b795b7bf7ee1a7dcfec2adc4a62eb2776df3e
masarakki <masaki@hisme.net>
2013-06-15 14:07:12 +0900

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifePlan::Application.config.secret_token = '519b7adb4a4fc233a35203e227db6a6623f936e2df4fb7835537c7587bb1c69d1c919cdc87a0267fcb009e01630abdf590539b3a0669fb8fa9578abbe9b2085b'
